835c248e3db5e356d42601e95753e385910b942c
Offirmo <offirmo.net@gmail.com>
2016-10-25 14:38:18 +0200

    +

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # hello-world-webextension
+
+The simplest, most trivial browser extension, featuring:
+* content script (aka. injected)
+* background script
+* toolbar button opening a popup
+* options page
+* notification
+* ...
+
+## references
+* https://developer.mozilla.org/en-US/Add-ons/WebExtensions/Your_first_WebExtension
+* https://developer.mozilla.org/en-US/Add-ons/WebExtensions/Your_second_WebExtension
+* https://developer.chrome.com/extensions/getstarted
--- background.js
+++ background.js
@@ -1,1 +1,21 @@
 console.log('Hello from hello-world-webextension (background)')
+
+chrome.contextMenus.create({
+	id: "hello-world-webextension-1",
+	title: "option 1",
+	contexts: ["all"]
+})
+
+chrome.contextMenus.create({
+	id: "hello-world-webextension-2",
+	title: "option 2",
+	contexts: ["all"]
+})
+
+var notification_id = "cake-notification"
+chrome.notifications.create(notification_id, {
+	"type": "basic",
+	"iconUrl": chrome.extension.getURL("icons/icon_48x48.png"),
+	"title": "hello-world-webextension notification",
+	"message": "hello-world-webextension notif from background"
+})
