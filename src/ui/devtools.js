af69555479b80d6f31f5dd66b6c8c7c96ec1b700
Offirmo <offirmo.net@gmail.com>
2019-03-01 14:25:03 +1100

    cross browser devtools pane

--- src/ui/devtools.js
+++ src/ui/devtools.js
@@ -2,25 +2,39 @@ console.log(`🧩 [T=${+Date.now()}] Hello from devtools!`, {
 	panels: chrome.devtools.panels,
 })
 
+function get_browser() {
+	if (typeof chrome !== 'undefined') {
+		if (typeof browser !== 'undefined')
+			return 'Firefox'
+
+		return 'Chrome'
+	}
+
+	return 'Other'
+}
+
 // only this script has access to the devtools API
 // https://developer.chrome.com/extensions/devtools
 
 chrome.devtools.inspectedWindow.eval(
 	"console.log(`🧩 [T=${+Date.now()}] Hello from injected from devtools!`)",
 	function(result, isException) { }
-);
+)
 
 const PANEL_NAME = 'Offirmo'
 chrome.devtools.panels.create(
 	PANEL_NAME,
-	"icons/icon_48x48.png",
-	"ui/devtools-panel.html",
-	function(panel) {
+	'../icons/icon_48x48.png', // works only on FF
+	// Chrome and FF have different paths
+	get_browser() === 'Firefox'
+		? './devtools-panel.html'
+		: 'ui/devtools-panel.html',
+	(panel) => {
 		// code invoked on panel creation
-		console.log(`🧩 [T=${+Date.now()}] Hello from devtools panel "${PANEL_NAME}" creation!`, panel, chrome.devtools.panels.ExtensionPanel)
+		console.log(`🧩 [T=${+Date.now()}] Hello from devtools panel "${PANEL_NAME}" creation!`, panel)
 
 		setTimeout(() => {
-			console.log('panels = ', chrome.devtools.panels)
+			console.log('devtools panels = ', chrome.devtools.panels)
 		}, 5000)
 		/*panel.createSidebarPane(
 				"Offirmo Sidebar",
@@ -30,5 +44,4 @@ chrome.devtools.panels.create(
 				}
 		  );*/
 	}
-);
-
+)
